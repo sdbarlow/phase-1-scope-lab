5b64213edf0abb14b1e14b9c944a487eab91845c
Seth Barlow <sethdbarlow@gmail.com>
2023-01-30 16:55:03 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    //console.log(customerName)
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "david";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "mike";
+}
